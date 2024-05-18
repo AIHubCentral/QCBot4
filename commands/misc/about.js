a6d13fc4220986cf95f2c92e303ee8936a52b61a
Antasma245 <144015963+Antasma245@users.noreply.github.com>
2024-05-18 16:19:40 +0200

    Changed repository link in '/about'

--- commands/misc/about.js
+++ commands/misc/about.js
@@ -11,7 +11,7 @@ module.exports = {
                 .setThumbnail(interaction.client.user.avatarURL())
                 .setDescription(`## I'm QCBot. Nice to meet you!
                     \n- Made by <@1037338320960761998>, with lots of help from the amazing AI HUB team!
-                    \n- Did you know I'm open source? [Well there you go.](https://github.com/Antasma245/QCBot)
+                    \n- Did you know I'm open source? [Well there you go.](https://github.com/AIHubCentral/QCBot4)
                     \n- Got a model to submit? Feel free to try out my commands!`) ]
         });
 	}
