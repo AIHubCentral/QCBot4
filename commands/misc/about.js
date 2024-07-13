c6c47911c3ded7de9c0669a7af622a31914f11a5
Antasma245 <144015963+Antasma245@users.noreply.github.com>
2024-07-13 17:11:07 +0200

    Removed user mention

--- commands/misc/about.js
+++ commands/misc/about.js
@@ -10,7 +10,7 @@ module.exports = {
                 .setColor(`6ba4b8`)
                 .setThumbnail(interaction.client.user.avatarURL())
                 .setDescription(`## I'm QCBot. Nice to meet you!
-                    \n- Made by <@1037338320960761998>, with lots of help from the amazing AI HUB team!
+                    \n- Made by **Antasma**, with lots of help from the amazing AI HUB team!
                     \n- Did you know I'm open source? [Well there you go.](https://github.com/AIHubCentral/QCBot4)
                     \n- Got a model to submit? Feel free to try out my commands!`) ]
         });
